94597c6479aa2309295b06033b6511f598c9fa2a
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-09-22 11:01:26 -0300

    Add Layout component

---
+++ src/components/Layout.vue
@@ -0,0 +1,29 @@
+<script lang='ts'>
+  export default {
+    name: 'ContentLayout',
+    props: {
+      title: String,
+      subtitle: String
+    }
+  }
+</script>
+
+<template>
+  <div class='layout container-xxl'>
+    <h1>{{ title }}</h1>
+    <h5>{{ subtitle }}</h5>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+  .layout {
+    padding-top: 50px;
+  }
+  h1 {
+    margin-bottom: 20px;
+  }
+  h5 {
+    margin-bottom: 35px;
+  }
+</style>
