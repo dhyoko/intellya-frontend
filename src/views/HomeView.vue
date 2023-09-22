fbb02a251b57384e33c89d3b4e1f75218dd192ab
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-09-22 14:06:40 -0300

    Add home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,55 @@
-<script setup lang="ts">
+<script lang="ts">
+  import api from '@/libs/axios';
+  import type { User } from '@/utils/types';
+  import { defineComponent } from 'vue';
+  import Layout from '@/components/Layout.vue';
+  import UserTable from '@/components/UserTable.vue';
+
+  export default defineComponent({
+    name: 'HomeView',
+    data() {
+      return {
+        userList: [] as User[],
+        isLoading: false,
+        errorMessage: ""
+      }
+    },
+    components: {
+      Layout,
+      UserTable
+    },
+    async mounted() {
+      try {
+        this.isLoading = true;
+        const response = await this.fetchUserList();
+        this.userList = response?.data;
+      } catch(error) {
+        this.errorMessage = (error as Error).message;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    methods: {
+      async fetchUserList() {
+        return api.get('/users', {
+          headers: {
+            'x-api-key': import.meta.env.VITE_API_KEY
+          }
+        });
+      }
+    }
+  })
 </script>
 
 <template>
   <main>
-    <span>Home</span>
+    <Layout
+      title='Listagem de Usuário'
+      subtitle='Visualize os usuários que acessam sua plataforma.'
+    >
+      <UserTable
+        :user-list='userList'
+      ></UserTable>
+    </Layout>
   </main>
 </template>
