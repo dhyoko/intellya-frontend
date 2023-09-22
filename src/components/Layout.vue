10a38ceeb833f2f2ee671344fb7e62f21d6396d4
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-09-22 14:03:23 -0300

    Fix missing define component wrapper

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,11 +1,12 @@
 <script lang='ts'>
-  export default {
+  import { defineComponent } from 'vue';
+  export default defineComponent({
     name: 'ContentLayout',
     props: {
       title: String,
       subtitle: String
     }
-  }
+  })
 </script>
 
 <template>
